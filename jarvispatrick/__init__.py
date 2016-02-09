f1282db6189ecf353928e2fb4a3aed324c757bab
Leonardo Lazzaro <llazzaro@dc.uba.ar>
2016-02-08 23:43:51 -0300

    add simple unit test and bug fixing

--- jarvispatrick/__init__.py
+++ jarvispatrick/__init__.py
@@ -19,42 +19,50 @@ class JarvisPatrick(object):
 
     def __init__(self, dataset_elements, distance_function):
         self.dataset_elements = dataset_elements
-        # initially each element is a cluster of 1 element
-        self.cluster = {element: cluster_index for cluster_index, element in self.dataset_elements}
         self.distance_function = distance_function
+        # initially each element is a cluster of 1 element
+        self.cluster = {element: cluster_index for cluster_index, element in enumerate(self.dataset_elements)}
 
     def __call__(self, number_of_neighbors, number_of_common_neighbors):
         """
         """
-
-        neighbors = {}
+        if number_of_common_neighbors >= number_of_neighbors:
+            raise ValueError('Asked for more common neighbors than number of neighbors')
+        neighbors_list = {}
         for element in self.dataset_elements:
-            neighbors[element] = self.calculate_k_nearest_elements(element, number_of_neighbors)
-
-        for element, neighbors in neighbors.items():
-            for other_element, other_neighbors in neighbors.items():
+            neighbors_list[element] = self.calculate_k_nearest_elements(element, number_of_neighbors)[:number_of_neighbors]
+        for element, neighbors in neighbors_list.items():
+            for other_element, other_neighbors in neighbors_list.items():
                 if element != other_element:
                     # we check both sides since the relation is not symmetric
                     if element in other_neighbors and other_element in neighbors:
                         if len(set(neighbors).intersection(other_neighbors)) >= number_of_common_neighbors:
                             self.reconfigure_clusters(element, other_element)
 
+        return self.cluster
+
     def reconfigure_clusters(self, element, other_element):
         if self.cluster[element] != self.cluster[other_element]:
             # we keep the lowest index
             if self.cluster[other_element] > self.cluster[element]:
                 for cluster_element in self.dataset_elements:
-                    if self.cluster[cluster_element] == self.cluster[other_element]:
+                    if cluster_element != other_element and self.cluster[cluster_element] == self.cluster[other_element]:
                         self.cluster[cluster_element] = self.cluster[element]
                 self.cluster[other_element] = self.cluster[element]
             else:
                 for cluster_element in self.dataset_elements:
-                    if self.cluster[cluster_element] == self.cluster[element]:
+                    if cluster_element != element and self.cluster[cluster_element] == self.cluster[element]:
                         self.cluster[cluster_element] = self.cluster[other_element]
                 self.cluster[element] = self.cluster[other_element]
 
     def calculate_k_nearest_elements(self, element, k_value):
         res = []
         for neighbor in self.dataset_elements:
-            res.append((neighbor, self.distance_function(element, neighbor)))
-        return map(lambda element: element[1], sorted(res, key=lambda neighbor_tuple: neighbor_tuple[1]))[:k_value]
+            if neighbor == element:
+                continue
+            distance = self.distance_function(element, neighbor)
+            if distance < 0:
+                raise Exception('Distance function must return positive number')
+            res.append((neighbor, distance))
+        res = sorted(res, key=lambda neighbor_tuple: neighbor_tuple[1])
+        return [element] + map(lambda element: element[0], res)
--- tests/test_jarvispatrick.py
+++ tests/test_jarvispatrick.py
@@ -10,19 +10,39 @@ Tests for `jarvispatrick` module.
 
 import unittest
 
-from jarvispatrick import jarvispatrick
+from jarvispatrick import JarvisPatrick
 
 
-class TestJarvispatrick(unittest.TestCase):
+class TestJarvisPatrick(unittest.TestCase):
 
-    def setUp(self):
-        pass
+    def test_small_case(self):
 
-    def tearDown(self):
-        pass
+        def hardcoded_length(element, element2):
+            if element in [1, 2] and element2 in [1, 2]:
+                return 0.6
+            if element in [1, 3] and element2 in [1, 3]:
+                return 0.55
+            if element in [2, 3] and element2 in [2, 3]:
+                return 0.67
+            if element in [2, 4] and element2 in [2, 4]:
+                return 0.9
+            if element in [4, 5] and element2 in [4, 5]:
+                return 0.6
+            if element in [4, 6] and element2 in [4, 6]:
+                return 0.57
+            if element in [5, 6] and element2 in [5, 6]:
+                return 0.7
+            if element in [5, 8] and element2 in [5, 8]:
+                return 0.88
+            if element in [3, 8] and element2 in [3, 8]:
+                return 0.9
 
-    def test_000_something(self):
-        pass
+            return 1.0
+
+        elements = [1, 2, 3, 4, 5, 6, 7, 8]
+        clustering = JarvisPatrick(elements, hardcoded_length)
+        cluster = clustering(3, 2)
+        self.assertEquals(cluster, {1: 0, 2: 0, 3: 0, 4: 3, 5: 3, 6: 3, 7: 6, 8: 7})
 
 
 if __name__ == '__main__':
