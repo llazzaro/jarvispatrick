115f13195a970c967cdd5ec84a15122f9b4ef193
Leonardo Lazzaro <llazzaro@dc.uba.ar>
2016-02-09 01:41:44 -0300

    version increase

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='jarvispatrick',
-    version='0.1.0',
+    version='0.1.1',
     description="Jarvis-Patrick Clustering that uses a nearest neighbor approach to clustering objects",
     long_description=readme + '\n\n' + history,
     author="Leonardo Lazzaro",
